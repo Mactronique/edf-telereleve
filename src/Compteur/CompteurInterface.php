95ee569f1f1d2dfc73c76e1eb169240570a5a77a
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2016-04-25 22:47:47 +0200

    dev

--- src/Compteur/CompteurInterface.php
+++ src/Compteur/CompteurInterface.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * This file is part of Mactronique EDF TeleReleve package.
+ *
+ * @author Jean-Baptiste Nahan <jbnahan@gmail.com>
+ * @copyright 2016 - Jean-Baptiste Nahan
+ * @license MIT
+ */
 namespace Mactronique\TeleReleve\Compteur;
 
 interface CompteurInterface
@@ -8,4 +14,9 @@ interface CompteurInterface
      * @return ReleveInterface
      */
     public function read();
+
+    /**
+     * @param string $device
+     */
+    public function defineDevicePath($device);
 }
--- src/Compteur/ReleveInterface.php
+++ src/Compteur/ReleveInterface.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * This file is part of Mactronique EDF TeleReleve package.
+ *
+ * @author Jean-Baptiste Nahan <jbnahan@gmail.com>
+ * @copyright 2016 - Jean-Baptiste Nahan
+ * @license MIT
+ */
 namespace Mactronique\TeleReleve\Compteur;
 
 interface ReleveInterface
